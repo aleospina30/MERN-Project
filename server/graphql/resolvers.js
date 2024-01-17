2ddeb16f61da8b0e10f6c3994152469b8787a3f6
alejospina30 <alejospina08@gmail.com>
2024-01-17 15:52:01 -0500

    Se hace la relación de uno a muchos en Task y Projects

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -60,4 +60,11 @@ export const resolvers = {
       return updatedTask;
     }
   },
+  Project: {
+    tasks: async (parent) => await Task.find({ projectId: parent._id }),
+  },
+  Task: {
+    project: async (parent) => await Project.findById(parent.projectId)
+  }
+
 };
