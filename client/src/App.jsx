0bad21a1c0a1919d6916dbdc904234b92c8dd6d5
alejospina30 <alejospina08@gmail.com>
2024-02-05 17:47:57 -0500

    feat(client): Se reemplaza la carpeta client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,34 @@
-import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import {projectModels} from './pages/projectModels';
-import { projectModelDetails }   from './pages/projectModelDetails'
- 
-
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache(),
-});
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <ApolloProvider client={client}>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Navigate to="/projectModels"/>}/>
-        <Route path='/projectModels' element={<projectModels/>}/>
-        <Route path='/projectModels/:id' element={<projectModelDetails/>}/>
-      </Routes>
-      </BrowserRouter>
-    </ApolloProvider>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
--- client/src/components/ProjectForm.jsx
+++
@@ -1,5 +0,0 @@
-export function projectModelForm() {
-  return (
-    <div>projectModelForm</div>
-  )
-}
