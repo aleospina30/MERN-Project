023cc9d64b4ef46f9a9d34a4d52d2606514e8b01
alejospina30 <alejospina08@gmail.com>
2024-02-05 10:32:45 -0500

    fix(verify.js): se cambia el console.log por return

--- server/middlewares/verify.js
+++ server/middlewares/verify.js
@@ -7,6 +7,6 @@ export const verifyToken = (token, secret) => {
     const session = jwt.verify(token, secret);
     return session
   } catch (error) {
-    console.log(error.message);
+    return error;
   }
 };
