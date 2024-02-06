578c9e5e9b3c238bd97c82b53ae521cd38baabe4
alejospina30 <alejospina08@gmail.com>
2024-02-06 10:50:59 -0500

    feat(package.json, App.jsx): Se instalan librerias y se modifica el App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,21 @@
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
+ const client = new ApolloClient({
+  uri: 'https://localhost:4000',
+  cache: new InMemoryCache()
+})
+
 function App() {
     return (
-      <div> Hello Sejotaz</div>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+        <Routes>
+        <Route></Route>
+        </Routes>
+        </BrowserRouter>
+      </ApolloProvider>
     )
   }
 
