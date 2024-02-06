24445bd36053658cbe2870c336a624686482d321
alejospina30 <alejospina08@gmail.com>
2024-02-06 13:51:54 -0500

    Se vuelve a crear una proyecto de client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,25 @@
-import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Projects } from "./pages/Projects";
+import { ProjectDetails } from "./pages/ProjectDetails";
 
 
- const client = new ApolloClient({
-  uri: 'https://localhost:4000',
-  cache: new InMemoryCache()
-})
+
+const client = new ApolloClient({
+  uri: "https://localhost:4000",
+  cache: new InMemoryCache(),
+});
 
 function App() {
-    return (
-      <ApolloProvider client={client}>
-        <BrowserRouter>
-        <Routes>
-        <Route></Route>
-        </Routes>
-        </BrowserRouter>
-      </ApolloProvider>
-    )
-  }
-
-export default App
-
+  return (
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/projects/:id" element={<ProjectDetails/>}/>
+      </Routes>
+    </BrowserRouter>
+  </ApolloProvider>
+  );
+}
+export default App;
